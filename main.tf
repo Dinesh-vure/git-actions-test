627908cdf97b09ae4da53da424ece9acaab4f09d
Your Name <dinesh.vures@gmail.com>
2026-06-26 14:52:55 +0530

    Fixed main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-main.tf
-
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "server" {
-
+  ami           = "ami-0c55b159cbfaa5888"
+  instance_type = "t2.micro"
 }
